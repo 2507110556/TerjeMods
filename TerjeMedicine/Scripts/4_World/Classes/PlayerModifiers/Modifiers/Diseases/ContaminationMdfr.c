3e7bc5cc635473ad9610d96ea540998d5522d38b
Streatman <TheStreatman@gmail.com>
2026-03-04 12:10:31 +0100

    Fix 'Missing function scope' for 1.29

--- TerjeMedicine/Scripts/4_World/Classes/PlayerModifiers/Modifiers/Diseases/ContaminationMdfr.c
+++ TerjeMedicine/Scripts/4_World/Classes/PlayerModifiers/Modifiers/Diseases/ContaminationMdfr.c
@@ -1,7 +1,7 @@
 modded class ContaminationStage1Mdfr
 {
-	override protected bool ActivateCondition(PlayerBase player) return false;
-	override protected bool DeactivateCondition(PlayerBase player) return false;
+	override protected bool ActivateCondition(PlayerBase player) {return false;}
+	override protected bool DeactivateCondition(PlayerBase player) {return false;}
 	override protected void OnActivate(PlayerBase player);
 	override protected void OnDeactivate(PlayerBase player);
 	override void OnTick(PlayerBase player, float deltaT);
@@ -9,8 +9,8 @@ modded class ContaminationStage1Mdfr
 
 modded class ContaminationStage2Mdfr
 {
-	override protected bool ActivateCondition(PlayerBase player) return false;
-	override protected bool DeactivateCondition(PlayerBase player) return false;
+	override protected bool ActivateCondition(PlayerBase player) {return false;}
+	override protected bool DeactivateCondition(PlayerBase player) {return false;}
 	override protected void OnActivate(PlayerBase player);
 	override protected void OnDeactivate(PlayerBase player);
 	override void OnTick(PlayerBase player, float deltaT);
@@ -18,8 +18,8 @@ modded class ContaminationStage2Mdfr
 
 modded class ContaminationStage3Mdfr
 {
-	override protected bool ActivateCondition(PlayerBase player) return false;
-	override protected bool DeactivateCondition(PlayerBase player) return false;
+	override protected bool ActivateCondition(PlayerBase player) {return false;}
+	override protected bool DeactivateCondition(PlayerBase player) {return false;}
 	override protected void OnActivate(PlayerBase player);
 	override protected void OnDeactivate(PlayerBase player);
 	override void OnTick(PlayerBase player, float deltaT);
